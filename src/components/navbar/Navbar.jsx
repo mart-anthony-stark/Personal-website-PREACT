7a15eade209f0680ce25bee772681a030368afb1
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-22 19:59:39 +0800

    add: navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+  return (
+    <nav>
+      <h2 className="logo">M</h2>
+      <div className="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Skills</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
+    </nav>
+  );
+}
