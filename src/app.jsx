a34ff96caf54aab13b297e9d91659c353cdd4f97
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-22 19:59:31 +0800

    remove: nav

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,7 @@
 export function App(props) {
   return (
     <>
-      <nav>
-        <h2 className="logo">M</h2>
-        <div className="menu">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Skills</a>
-          <a href="#">Projects</a>
-          <a href="#">Contact</a>
-        </div>
-      </nav>
+    
     </>
   );
 }
