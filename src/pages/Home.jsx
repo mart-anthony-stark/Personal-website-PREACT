3232db42448c0927377fc1b665391a39ae38acd9
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-04 10:17:42 +0800

    add banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/navbar/Navbar";
+import Banner from "../components/banner/Banner";
 
 export default function Home(props) {
   return (
     <>
-      <Navbar></Navbar>
-      
+      <Navbar />
+      <Banner />
     </>
   );
 }
