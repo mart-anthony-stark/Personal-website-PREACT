e0f2e4ac46a033910369d61d8483b828ad4079e5
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-04 10:17:08 +0800

    remove banner elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,26 +4,7 @@ export default function Home(props) {
   return (
     <>
       <Navbar></Navbar>
-      <h2>Hello there! I'm</h2>
-      <h1 class="logo" data-text="Mart_Anthony">
-        Mart<span>_Anthony</span>
-      </h1>
-      <h2 class="desc">
-        I am <span></span>
-      </h2>
-      <a
-        href="https://github.com/mart-anthony-stark"
-        target="_blank"
-        class="github"
-      >
-        <i class="fa fa-github-square" aria-hidden="true"></i>
-        GitHub
-      </a>
-      <a href="#profile" class="scroll-down">
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
+      
     </>
   );
 }
