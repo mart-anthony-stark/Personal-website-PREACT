c33d909e3f20acba0b0fa1ce5ded7b9a2cfecdb8
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-07 22:19:01 +0800

    add socials

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -1,6 +1,33 @@
 import "./socials.css";
 
 const Socials = () => {
-  return <div className="socials">asdasdasd</div>;
+  return (
+    <div className="socials left-items col gap-2">
+      <a
+        href="https://github.com/mart-anthony-stark"
+        target="_blank"
+        class="social-link center-items gap-2"
+      >
+        <i class="fa fa-github-square" aria-hidden="true"></i>
+        GitHub
+      </a>
+      <a
+        href="https://github.com/mart-anthony-stark"
+        target="_blank"
+        class="social-link center-items gap-2"
+      >
+        <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+        LinkedIn
+      </a>
+      <a
+        href="https://github.com/mart-anthony-stark"
+        target="_blank"
+        class="social-link center-items gap-2"
+      >
+        <i class="fa fa-facebook-square" aria-hidden="true"></i>
+        Facebook
+      </a>
+    </div>
+  );
 };
 export default Socials;
