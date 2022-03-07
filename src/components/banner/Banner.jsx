0da15136ef1230cdbe13bb1d2db0b9e5e4eb4f6b
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-07 22:18:37 +0800

    remove social links

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -11,32 +11,6 @@ export default function Banner() {
         <h2 class="desc">
           I am <span></span>
         </h2>
-        <div className="center-items gap-2">
-          <a
-            href="https://github.com/mart-anthony-stark"
-            target="_blank"
-            class="social-link center-items gap-2"
-          >
-            <i class="fa fa-github-square" aria-hidden="true"></i>
-            GitHub
-          </a>
-          <a
-            href="https://github.com/mart-anthony-stark"
-            target="_blank"
-            class="social-link center-items gap-2"
-          >
-            <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            LinkedIn
-          </a>
-          <a
-            href="https://github.com/mart-anthony-stark"
-            target="_blank"
-            class="social-link center-items gap-2"
-          >
-            <i class="fa fa-facebook-square" aria-hidden="true"></i>
-            Facebook
-          </a>
-        </div>
       </div>
     </>
   );
