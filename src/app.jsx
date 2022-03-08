4729e76266b4e666523003d762ce7dd18a6304c9
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-08 19:29:12 +0800

    add effect to mouse cursor

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,45 @@
 import Home from "./pages/Home";
 import Router from "preact-router";
+import { useEffect } from "preact/hooks";
 
 export function App(props) {
+  useEffect(() => {
+    const cursor = document.querySelector("#cursor");
+    function followCursor(e) {
+      cursor.style.top = e.clientY + "px";
+      cursor.style.left = e.clientX + "px";
+    }
+    function parallax(e) {
+      document.querySelectorAll(".parallax-layer").forEach((layer) => {
+        const speed = layer.getAttribute("data-speed");
+
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        layer.style.transform = `translate(${x}px,${y}px)`;
+      });
+    }
+
+    function snap() {
+      cursor.classList.add("snap");
+
+      setTimeout(() => {
+        cursor.classList.remove("snap");
+      }, 500);
+    }
+
+    window.addEventListener("mousemove", followCursor);
+    window.addEventListener("mousemove", parallax);
+    window.addEventListener("click", snap);
+
+    return () => {
+      window.removeEventListener("mousemove", followCursor);
+      window.removeEventListener("mousemove", parallax);
+      window.removeEventListener("click", snap);
+    };
+  }, []);
   return (
     <>
+      <div id="cursor"></div>
       <Router>
         <Home path="/" />
       </Router>
