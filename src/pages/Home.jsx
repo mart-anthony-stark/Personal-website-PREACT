d5037a4593c6a5027555f381c2f88f2de4f414dc
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-09 10:16:45 +0800

    fixed export

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,17 @@ import Banner from "../components/banner/Banner";
 import Socials from "../components/socials/Socials";
 import Particles from "preact-particles";
 
-export default function Home(props) {
+const Home = (props) => {
   return (
     <>
       <Navbar />
       <main className="center-items">
-        <Particles
-          id="tsparticles"
-          url="/assets/js/particlesjs-config.json"
-        />
+        <Particles id="tsparticles" url="/assets/js/particlesjs-config.json" />
         <Banner />
         <Socials />
       </main>
     </>
   );
-}
+};
+
+export default Home;
