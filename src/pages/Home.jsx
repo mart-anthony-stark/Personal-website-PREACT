e57066386e53125f13d56e678a7367c98a182c40
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-09 10:31:06 +0800

    remove navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/navbar/Navbar";
 import Banner from "../components/banner/Banner";
 import Socials from "../components/socials/Socials";
 import Particles from "preact-particles";
@@ -6,7 +5,6 @@ import Particles from "preact-particles";
 const Home = (props) => {
   return (
     <>
-      <Navbar />
       <main className="center-items">
         <Particles id="tsparticles" url="/assets/js/particlesjs-config.json" />
         <Banner />
