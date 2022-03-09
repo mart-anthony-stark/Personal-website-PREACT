01b243c1f1c026687f3861bfa41091a15350e9cb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-09 10:39:26 +0800

    add router links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import "./navbar.css";
+import { Link } from "preact-router/match";
 
 export default function Navbar() {
   return (
     <nav>
       <h2 className="logo">Mart</h2>
       <div className="menu">
-        <a href="#">Home</a>
-        <a href="#">About</a>
+        <Link activeClassName="active" href="/">
+          Home
+        </Link>
+        <Link activeClassName="active" href="/about">
+          About
+        </Link>
         <a href="#">Skills</a>
         <a href="#">Projects</a>
         <a href="#">Contact</a>
