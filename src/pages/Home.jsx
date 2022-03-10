980ead62802d5fc80f943cf8328c25eaa54be960
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-10 20:43:53 +0800

    add redirect mouse wheel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import Banner from "../components/banner/Banner";
 import Socials from "../components/socials/Socials";
 import Particles from "preact-particles";
+import { route } from "preact-router";
 
 const Home = (props) => {
+  const handleMouseWheel = (e) => {
+    if (e.wheelDeltaY == -120) {
+      route("/about");
+    }
+  };
+
   return (
     <>
-      <main className="center-items">
+      <main className="center-items" onWheel={handleMouseWheel}>
         <Particles id="tsparticles" url="/assets/js/particlesjs-config.json" />
         <Banner />
         <Socials />
