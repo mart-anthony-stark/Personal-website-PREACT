01b6da2828d582f30140a3d002189959a332d911
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-11 20:31:42 +0800

    add key props

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       <div className="container">
         {projects.map((project) => {
           return (
-            <div className="project">
+            <div className="project" key={project.id}>
               <img src={project.img} />
             </div>
           );
@@ -18,33 +18,52 @@ const Projects = () => {
 
 const projects = [
   {
+    id: 1,
     img: "/assets/projects/react-os.png",
   },
   {
+    id: 2,
+
     img: "/assets/projects/ShoeShop.png",
   },
   {
+    id: 3,
+
     img: "/assets/projects/techzen.png",
   },
   {
+    id: 4,
+
     img: "/assets/projects/weather.png",
   },
   {
+    id: 5,
+
     img: "/assets/projects/covidtracker.png",
   },
   {
+    id: 6,
+
     img: "/assets/projects/exotify.png",
   },
   {
+    id: 7,
+
     img: "/assets/projects/jaja.png",
   },
   {
+    id: 8,
+
     img: "/assets/projects/landingPage.png",
   },
   {
+    id: 9,
+
     img: "/assets/projects/News.png",
   },
   {
+    id: 10,
+
     img: "/assets/projects/Pizza.png",
   },
 ];
