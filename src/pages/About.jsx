bc7077113f3969bac6975b552076f2e9c93f3572
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-11 19:34:05 +0800

    remove skills section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,5 @@
+import Skills from "../components/skills/Skills";
 import "./styles/about.css";
-import { Doughnut } from "react-chartjs-2";
-import { Chart, registerables } from "chart.js";
-import "chartjs-plugin-datalabels";
-Chart.register(...registerables);
 
 const About = () => {
   return (
@@ -23,36 +20,7 @@ const About = () => {
           <img src="/assets/SALAZAR.png" alt="Mart Anthony A. Salazar" />
         </div>
       </div>
-      <section id="skills">
-        <Doughnut
-          datasetIdKey="id"
-          options={{
-            plugins: {
-              title: {
-                display: true,
-                text: "Programming Language Proficiency",
-              }
-            },
-          }}
-          data={{
-            title: "a",
-            labels: ["JavaScript", "Java", "C++", "Python"],
-            datasets: [
-              {
-                label: "Languages",
-                data: [98, 70, 60, 80],
-                backgroundColor: [
-                  "rgb(255, 205, 86)",
-                  "rgb(54, 162, 235)",
-                  "rgb(255, 99, 132)",
-                  "rgb(86, 255, 94)",
-                ],
-                hoverOffset: 4,
-              },
-            ],
-          }}
-        />
-      </section>
+      <Skills />
     </>
   );
 };
