a10f40c472bcab2ee18c604087f99db476dde30e
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-11 20:01:16 +0800

    add projects data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,44 @@
 const Projects = () => {
-  return <div className="projects"></div>;
+  return (
+    <div className="projects">
+      {projects.map((project) => {
+        return <img src={project.img} />;
+      })}
+    </div>
+  );
 };
+
+const projects = [
+  {
+    img: "/assets/projects/react-os.png",
+  },
+  {
+    img: "/assets/projects/ShoeShop.png",
+  },
+  {
+    img: "/assets/projects/techzen.png",
+  },
+  {
+    img: "/assets/projects/weather.png",
+  },
+  {
+    img: "/assets/projects/covidtracker.png",
+  },
+  {
+    img: "/assets/projects/exotify.png",
+  },
+  {
+    img: "/assets/projects/jaja.png",
+  },
+  {
+    img: "/assets/projects/landingPage.png",
+  },
+  {
+    img: "/assets/projects/News.png",
+  },
+  {
+    img: "/assets/projects/Pizza.png",
+  },
+];
 
 export default Projects;
